9cdbd573b749bbd4024f3a9198ecbed4409f1b87
cskincses <cskincses@gmail.com>
2023-03-11 15:21:29 +0100

    Location foreign key for ProductID added

--- DataAccessLayer/DataTable/Location.cs
+++ DataAccessLayer/DataTable/Location.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,18 @@ namespace DataAccessLayer.DataTable
 {
     public class Location
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int ID { get; set; }
+
+        [Required]
         public int ProductID { get; set; }
         public string ProductName { get; set; }
         public int NumberOfProducts { get; set; }
         public int MaxNumberOfProductPerCell { get; set; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { get; set; }
 
     }
 }
---
+++ DataAccessLayer/Migrations/20230311141407_Location specified.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProductManagement.MVC.Data.Migrations
+{
+    public partial class Locationspecified : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProjectStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    projectID = table.Column<int>(type: "int", nullable: false),
+                    projectCurrentStat = table.Column<int>(type: "int", nullable: false),
+                    statusChanged = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectStatuses", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Locations_ProductID",
+                table: "Locations",
+                column: "ProductID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Locations_Products_ProductID",
+                table: "Locations",
+                column: "ProductID",
+                principalTable: "Products",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Locations_Products_ProductID",
+                table: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "ProjectStatuses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Locations_ProductID",
+                table: "Locations");
+        }
+    }
+}
