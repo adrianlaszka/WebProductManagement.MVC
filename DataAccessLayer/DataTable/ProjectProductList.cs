ebaee912bdd671e8b57df67b1a6ba76800c61a13
cskincses <cskincses@gmail.com>
2023-03-11 19:48:53 +0100

    ProjectProductList table added

---
+++ DataAccessLayer/DataTable/ProjectProductList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.DataTable
+{
+    public class ProjectProductList
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        [Required]
+        public int productID { get; set; }
+        public string projectID { get; set; }
+
+        [ForeignKey("ProductID")]
+        public virtual Product Product { get; set; }
+
+        [ForeignKey("ProjectID")]
+        public virtual ProjectDetails ProjectDetails { get; set; }
+
+
+    }
+}
