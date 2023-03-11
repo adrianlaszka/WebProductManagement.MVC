da8bf0cc8eff5fd0a7b00c601a5365cb14a47a2a
Adrián Péter Lászka <adrian.laszka90@gmail.com>
2023-03-11 15:56:09 +0100

    Migration Change

---
+++ DataAccessLayer/Migrations/20230311134238_Warehouses & Projectstatutes tables added.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebProductManagement.MVC.Data.Migrations
+{
+    public partial class WarehousesProjectstatutestablesadded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProjectStatuses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    projectID = table.Column<int>(type: "int", nullable: false),
+                    projectCurrentStat = table.Column<int>(type: "int", nullable: false),
+                    statusChanged = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectStatuses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectStatuses");
+        }
+    }
+}
